f9ccc8d29a533e6b61f092db324ffa9f1fb1e5e8
netanel22 <netanel.alon.09@gmail.com>
2013-07-25 14:03:04 +0000

    changed web.js2

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+//my new code
+var fs=require("fs");
+var fname="index.html";
+//
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFile(fname,'utf8'));
 });
 
 var port = process.env.PORT || 5000;
